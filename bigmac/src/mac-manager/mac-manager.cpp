9f3de6cd5c39ddf83a2e36f2848d1bb3692595f6
boven <boven.yan@gmail.com>
2016-04-02 18:57:29 -0400

    add mac-manager.cpp

---
+++ bigmac/src/mac-manager/mac-manager.cpp
@@ -0,0 +1,10 @@
+#include "mac-manager.h"
+#include <boost/algorithm/string.hpp>
+
+using boost::split;
+using boost::is_any_of;
+
+mac_manager::mac_manager(string config_file){
+    // parse config file
+    
+}
